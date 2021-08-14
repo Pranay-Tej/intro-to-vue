ca470b7fbc804b91be01ff8c821e3838f7604301
pranay_teja <s.pranayteja@gmail.com>
2021-08-14 19:27:22 +0530

    extract counter to component

--- src/App.vue
+++ src/App.vue
@@ -1,67 +1,18 @@
 <template>
-  <!-- :class="isNegative ? 'negative' : 'positive'" -->
-  <!-- :class="calculateClass()" -->
-  <p class="count" :class="isNegative ? 'negative' : 'positive'">
-    {{ count }}
-  </p>
-
-  <button @click="increase">⬆️ {{ step }}</button>
-  <button @click="decrease">⬇️ {{ step }}</button>
-  <button @click="reset">🚫</button>
-
-  <!-- TODO: use number input to bind 'step' -->
-  <br />
-  <label for="step">Step</label>
-  <input type="number" name="step" id="step" v-model="step" />
+  <Counter />
   <hr />
   <Forms />
 </template>
 
 <script>
 import Forms from "./views/Forms.vue";
+import Counter from "./views/Counter.vue";
 
 export default {
   name: "App",
   components: {
     Forms,
-  },
-  data() {
-    return {
-      count: 0,
-      step: 1,
-    };
-  },
-  methods: {
-    increase() {
-      console.log({
-        count: this.count,
-        msg: `increasing count by ${this.step} units`,
-      });
-      this.count += this.step;
-    },
-
-    // decrease count
-    decrease() {
-      this.count -= this.step;
-    },
-
-    // reset count
-    reset() {
-      this.count = 0;
-    },
-
-    // calculate if count is negative
-    calculateClass() {
-      console.log("method: runs if ANY data changes");
-      return this.count < 0 ? true : false;
-    },
-  },
-
-  computed: {
-    isNegative() {
-      console.log("computed: runs only when dependencies(count) change");
-      return this.count < 0 ? true : false;
-    },
+    Counter,
   },
 };
 </script>
@@ -74,18 +25,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-
-.count {
-  font-size: 3rem;
-  font-weight: bold;
-}
-
-.positive {
-  color: hsl(200, 60%, 40%);
-}
-
-.negative {
-  color: hsl(0, 60%, 50%);
 }
 </style>
---
+++ src/views/Counter.vue
@@ -0,0 +1,75 @@
+<template>
+  <!-- :class="isNegative ? 'negative' : 'positive'" -->
+  <!-- :class="calculateClass()" -->
+  <p class="count" :class="isNegative ? 'negative' : 'positive'">
+    {{ count }}
+  </p>
+
+  <button @click="increase">⬆️ {{ step }}</button>
+  <button @click="decrease">⬇️ {{ step }}</button>
+  <button @click="reset">🚫</button>
+
+  <!-- TODO: use number input to bind 'step' -->
+  <br />
+  <label for="step">Step</label>
+  <input type="number" name="step" id="step" v-model="step" />
+</template>
+
+<script>
+export default {
+  name: "Counter",
+  data() {
+    return {
+      count: 0,
+      step: 1,
+    };
+  },
+  methods: {
+    increase() {
+      console.log({
+        count: this.count,
+        msg: `increasing count by ${this.step} units`,
+      });
+      this.count += this.step;
+    },
+
+    // decrease count
+    decrease() {
+      this.count -= this.step;
+    },
+
+    // reset count
+    reset() {
+      this.count = 0;
+    },
+
+    // calculate if count is negative
+    calculateClass() {
+      console.log("method: runs if ANY data changes");
+      return this.count < 0 ? true : false;
+    },
+  },
+
+  computed: {
+    isNegative() {
+      console.log("computed: runs only when dependencies(count) change");
+      return this.count < 0 ? true : false;
+    },
+  },
+};
+</script>
+
+<style>
+.count {
+  font-size: 3rem;
+  font-weight: bold;
+}
+
+.positive {
+  color: hsl(200, 60%, 40%);
+}
+
+.negative {
+  color: hsl(0, 60%, 50%);
+}
+</style>
