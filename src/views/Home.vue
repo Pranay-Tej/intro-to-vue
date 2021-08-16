7d44c585056e858f23873c684a77ac37bec7ae2c
pranay_teja <s.pranayteja@gmail.com>
2021-08-16 11:47:03 +0530

    add CounterComposition component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,23 @@
 <template>
   <div>Intro to Vue</div>
+  <CounterComposition
+    :step="10"
+    @emitCurrentValue="printCounterValue(1, $event)"
+  />
 </template>
 
 <script>
-export default {};
+import CounterComposition from "./CounterComposition.vue";
+export default {
+  components: {
+    CounterComposition,
+  },
+  methods: {
+    printCounterValue(counterId, data) {
+      console.log({ counterId, ...data });
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
